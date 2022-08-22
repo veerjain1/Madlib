e26f82567c04b537e7ecb5aa4fca74821c610b2c
Veer Jain <111719388+veerjain1@users.noreply.github.com>
2022-08-22 22:59:53 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,75 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string adj1;
+
+    Console.WriteLine(" Please type an adjective here: ");
+    //store name as readline
+    adj1 = Console.ReadLine();//saved user input
+
+    string noun1;
+
+    Console.WriteLine("Please type a plural noun: ");
+    //store name as readline
+    noun1 = Console.ReadLine();//saved user input
+
+    string name;
+
+    Console.WriteLine("Please type a name: ");
+    //store name as readline
+    name = Console.ReadLine();//saved user input
+
+    string int1;
+
+    Console.WriteLine("Please type a number: ");
+    //store name as readline
+    int1 = Console.ReadLine();//saved user input
+
+    string verb1;
+
+    Console.WriteLine("Please type a verb ending with -ing : ");
+    //store name as readline
+    verb1 = Console.ReadLine();//saved user input
+
+    string noun2;
+
+    Console.WriteLine("Please type a new noun: ");
+    //store name as readline
+    noun2 = Console.ReadLine();//saved user input
+
+    string adj2;
+
+    Console.WriteLine("Please type a new adjective: ");
+    //store name as readline
+    adj2 = Console.ReadLine();//saved user input
+
+    string int2;
+
+    Console.WriteLine("Please type a new integer: ");
+    //store name as readline
+    int2 = Console.ReadLine();//saved user input
+
+    string noun3;
+
+    Console.WriteLine("Please type a new plural noun: ");
+    //store name as readline
+    noun3 = Console.ReadLine();//saved user input
+
+
+
+    string verb2;
+
+    Console.WriteLine("Please type a new verb ending with -ing : ");
+    //store name as readline
+    verb2 = Console.ReadLine();//saved user input
+
+    Console.WriteLine("\n\t\t\tMaking your story now...");
+
+    Console.WriteLine("A long long time ago, there was an [" +adj1+"] martian who ate way too many ["+noun1+"]. Their name was ["+name+"] .To be precise, every day ["+name+"] ate about ["+int1+"] of them! One day [" +name+"] was ["+verb1+"] down a valley and fell! Oh the Martian cried and cried all day long because of the darn [" +noun2+"]! Since ["+name+"] cried so much, his round head became all ["+adj2+"]! EW! The Martian yelled. All the sudden he saw ["+int2+"] ["+noun3+"] in the air ["+verb2+"] and coming straight for his toes! Uh Oh!");
+
+
+
+
+  }
+}
